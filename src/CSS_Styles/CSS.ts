e685b9679aacfab3a4df5f7147193da4ecf1c7a8
nolec <ca10205@naver.com>
2020-07-03 00:05:03 +0900

    Link add

--- src/CSS_Styles/CSS.ts
+++ src/CSS_Styles/CSS.ts
@@ -1,152 +1,157 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 interface ISectionProps {
-    position?: string
-    top?: string
-    left?: string
-    right?: string
-    zIndex?: number
-    display?: string
-    bgColor?: string
-    minWidth?: string
-    width?: string
-    height?: string
-    margin?: string
-    padding?: string
-    borderTop?: string
-    aI?: string
-    flexBasis?: string
-    textAlign?: string
+  position?: string;
+  top?: string;
+  left?: string;
+  right?: string;
+  zIndex?: number;
+  display?: string;
+  bgColor?: string;
+  minWidth?: string;
+  width?: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
+  borderTop?: string;
+  aI?: string;
+  flexBasis?: string;
+  textAlign?: string;
 }
 
 export const C_Section = styled.section<ISectionProps>`
-    position : ${props => props.position};
-    top : ${props => props.top};
-    left : ${props => props.left};
-    right : ${props => props.right};
-    z-index : ${props => props.zIndex};
-    display : ${props => props.display};
-    background-color ${props => props.bgColor};
-    min-width : ${props => props.minWidth};
-    width : ${props => props.width};
-    height : ${props => props.height};
-    margin : ${props => props.margin};
-    padding : ${props => props.padding};
-    border-top : ${props => props.borderTop};
-    align-items : ${props => props.aI};
-`
+    position : ${(props) => props.position};
+    top : ${(props) => props.top};
+    left : ${(props) => props.left};
+    right : ${(props) => props.right};
+    z-index : ${(props) => props.zIndex};
+    display : ${(props) => props.display};
+    background-color ${(props) => props.bgColor};
+    min-width : ${(props) => props.minWidth};
+    width : ${(props) => props.width};
+    height : ${(props) => props.height};
+    margin : ${(props) => props.margin};
+    padding : ${(props) => props.padding};
+    border-top : ${(props) => props.borderTop};
+    align-items : ${(props) => props.aI};
+`;
 export const C_ImgBox = styled.div<ISectionProps>`
-    width : ${props => props.width};
-    height : ${props => props.height};
-    margin : ${props => props.margin};
-    padding : ${props => props.padding};
-    flex-basis : ${props => props.flexBasis};
-`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  flex-basis: ${(props) => props.flexBasis};
+`;
 export const C_TextBox = styled.div<ISectionProps>`
-    width : ${props => props.width};
-    height : ${props => props.height};
-    margin : ${props => props.margin};
-    padding : ${props => props.padding};
-    flex-basis : ${props => props.flexBasis};
-    text-align: ${props => props.textAlign};
-`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  flex-basis: ${(props) => props.flexBasis};
+  text-align: ${(props) => props.textAlign};
+  flex-grow: 1;
+`;
 
 export interface IBannerCSSProps {
-    banner?: {
-        margin?: string
-        padding?: string
-    }
-    container?: {
-        width?: string
-        height?: string
-        padding?: string
-    }
-    contentBox?: {
-        margin?: string
-    }
-
+  banner?: {
+    margin?: string;
+    padding?: string;
+  };
+  container?: {
+    width?: string;
+    height?: string;
+    padding?: string;
+  };
+  contentBox?: {
+    margin?: string;
+  };
 }
 
 export interface IHeaderCSSProps extends IBannerCSSProps {
-    header?: { top?: string }
-    start?: {
-        width?: string
-    }
-    center?: {}
-    end?: {}
+  header?: { top?: string };
+  start?: {
+    width?: string;
+  };
+  center?: {};
+  end?: {};
 }
 
-const BannerCSS = ({banner, container, contentBox}: IBannerCSSProps) => css`
-    color : white;
-              padding : ${banner?.padding};
-    #container {
-        width : ${container?.width};
-        height : ${container?.height};
-        padding : ${container?.padding};
-        .content_box {
-          height : 100%;
-          background-color : #000000;
-          display : flex;
-          justify-content: center;
-          align-items: center;
-        }
+const BannerCSS = ({ banner, container, contentBox }: IBannerCSSProps) => css`
+  color: white;
+  padding: ${banner?.padding};
+  #container {
+    width: ${container?.width};
+    height: ${container?.height};
+    padding: ${container?.padding};
+    .content_box {
+      height: 100%;
+      background-color: #000000;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
+  }
 `;
-const HeaderCSS = ({header, container, start, center, end}: IHeaderCSSProps) => css`
-    position: fixed;
-    top : ${header?.top};
-    width : 100%;
-    background-color : #ffffff;
-    #container {
-        width : ${container?.width};        
-        height : ${container?.height};
-        padding : ${container?.padding};
-        display : flex;
-        justify-content: space-between;
-        align-items: center;
-        .icon_box {
-          width : 49px;
-          svg {
-            font-size: 50px;
-            width: 100%;
-          }
-        }       
-    }
-    #start {
-      .back {
-          display : block;
-          width : 49px;
-          height : 100%;
-       }
-    }
-    #center {
-    }
-    #end {
-      .content_box {
-          display : flex;
-          height : 100%; 
-          > a{
-            width : 43px;
-            padding : 0 4px;
-            margin : 0 6px;
-            .icon_box {
-              width : 100%;
-            }
-            &:first-child {
-              margin-left: 0;
-            }
-            &:last-child{
-              margin-right : 0;
-            }
-          }     
-
+const HeaderCSS = ({
+  header,
+  container,
+  start,
+  center,
+  end,
+}: IHeaderCSSProps) => css`
+  position: fixed;
+  top: ${header?.top};
+  width: 100%;
+  background-color: #ffffff;
+  #container {
+    width: ${container?.width};
+    height: ${container?.height};
+    padding: ${container?.padding};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .icon_box {
+      width: 49px;
+      svg {
+        font-size: 50px;
+        width: 100%;
       }
     }
+  }
+  #start {
+    .back {
+      display: block;
+      width: 49px;
+      height: 100%;
+    }
+  }
+  #center {
+  }
+  #end {
+    .content_box {
+      display: flex;
+      height: 100%;
+      > a {
+        width: 43px;
+        padding: 0 4px;
+        margin: 0 6px;
+        .icon_box {
+          width: 100%;
+        }
+        &:first-child {
+          margin-left: 0;
+        }
+        &:last-child {
+          margin-right: 0;
+        }
+      }
+    }
+  }
 `;
 const theme = {
-    css: {
-        banner: BannerCSS,
-        header: HeaderCSS
-    }
-}
+  css: {
+    banner: BannerCSS,
+    header: HeaderCSS,
+  },
+};
 export default theme;
--- src/components/Sections/SectionOne.tsx
+++ src/components/Sections/SectionOne.tsx
@@ -1,27 +1,29 @@
-import React, {useContext, useEffect} from "react";
-import {C_Section, C_ImgBox, C_TextBox} from "../../CSS_Styles/CSS";
+import React, { useContext, useEffect } from "react";
+import { C_Section, C_ImgBox, C_TextBox } from "../../CSS_Styles/CSS";
 import styled from "styled-components";
-import {ParsedUrlQuery} from "querystring";
-import {GetServerSidePropsContext} from "next";
-import {initializeApollo} from "../../lib/apolloClient";
-import {GET_ARTICLES} from "../../graphql";
+import { ParsedUrlQuery } from "querystring";
+import { GetServerSidePropsContext } from "next";
+import { initializeApollo } from "../../lib/apolloClient";
+import { GET_ARTICLES } from "../../graphql";
 import Context from "../../contextAPI/context";
 
 const Container = styled.div``;
 const ContentBox = styled.div`
-  display : flex;
+  display: flex;
   flex-wrap: nowrap;
 `;
 const Left = styled.div`
   flex-basis: 60%;
   border-right: solid 1px #e2e2e2;
-  padding : 0 20px 0px 20px;
+  padding: 0 20px 0px 20px;
+  display: flex;
+  flex-direction: column;
 `;
 const Right = styled.div`
   flex-basis: 40%;
-  display : flex;
+  display: flex;
   flex-direction: column;
-  padding : 0 20px 0px 20px;
+  padding: 0 20px 0px 20px;
 `;
 
 //--------Left--------------------------------
@@ -31,58 +33,61 @@ const Text = styled.p``;
 //--------Right--------------------------------
 const Top = styled.div`
   border-bottom: solid 1px #e2e2e2;
-  display : flex;
+  display: flex;
   flex-direction: column;
 `;
 const Bottom = styled.div`
-  display : flex;
-  padding : 20px 0 0 0
+  display: flex;
+  padding: 20px 0 0 0;
 `;
 
 const SectionOne = () => {
-    const {articlesData, articlesLoading} = useContext(Context);
+  const { articlesData, articlesLoading } = useContext(Context);
 
+  useEffect(() => {
+    if (articlesData) console.log(articlesData);
+  }, [articlesLoading]);
 
-    useEffect(() => {
-        if (articlesData) console.log(articlesData);
-    }, [articlesLoading])
-
-    return (
-        <C_Section margin={"70px 0 0 0"}>
-            <Container>
-                <ContentBox>
-                    <Left>
-                        <C_ImgBox>
-                            <img src={articlesData?.articles[0]?.squareThumbnail}/>
-                        </C_ImgBox>
-                        <C_TextBox padding={"20px 20px 0 20px"}  textAlign={"center"}>
-                            <Title>{articlesData?.articles[0]?.title}</Title>
-                            <Text>{articlesData?.articles[0]?.subtitle}</Text>
-                        </C_TextBox>
-                    </Left>
-                    <Right>
-                        <Top>
-                            <C_ImgBox flexBasis={"50%"}>
-                                <img src={articlesData?.articles[0]?.squareThumbnail}/>
-                            </C_ImgBox>
-                            <C_TextBox padding={"20px 20px 20px 20px"}  textAlign={"center"}>
-                                <Title>{articlesData?.articles[0]?.title}</Title>
-                                <Text>{articlesData?.articles[0]?.subtitle}</Text>
-                            </C_TextBox>
-                        </Top>
-                        <Bottom>
-                            <C_TextBox padding={"20px 20px 0 20px"} flexBasis={"50%"} textAlign={"auto"}>
-                                <Title>{articlesData?.articles[0]?.title}</Title>
-                                <Text>{articlesData?.articles[0]?.subtitle}</Text>
-                            </C_TextBox>
-                            <C_ImgBox flexBasis={"50%"}>
-                                <img src={articlesData?.articles[0]?.squareThumbnail}/>
-                            </C_ImgBox>
-                        </Bottom>
-                    </Right>
-                </ContentBox>
-            </Container>
-        </C_Section>
-    )
-}
+  return (
+    <C_Section margin={"70px 0 0 0"}>
+      <Container>
+        <ContentBox>
+          <Left>
+            <C_ImgBox>
+              <img src={articlesData?.articles[0]?.squareThumbnail} />
+            </C_ImgBox>
+            <C_TextBox padding={"20px 20px 0 20px"} textAlign={"center"}>
+              <Title>{articlesData?.articles[0]?.title}</Title>
+              <Text>{articlesData?.articles[0]?.subtitle}</Text>
+            </C_TextBox>
+          </Left>
+          <Right>
+            <Top>
+              <C_ImgBox flexBasis={"50%"}>
+                <img src={articlesData?.articles[0]?.squareThumbnail} />
+              </C_ImgBox>
+              <C_TextBox padding={"20px 20px 20px 20px"} textAlign={"center"}>
+                <Title>{articlesData?.articles[0]?.title}</Title>
+                <Text>{articlesData?.articles[0]?.subtitle}</Text>
+              </C_TextBox>
+            </Top>
+            <Bottom>
+              <C_TextBox
+                padding={"20px 20px 0 20px"}
+                flexBasis={"50%"}
+                textAlign={"auto"}
+              >
+                <Title>{articlesData?.articles[0]?.title}</Title>
+                <Text>{articlesData?.articles[0]?.subtitle}</Text>
+              </C_TextBox>
+              <C_ImgBox flexBasis={"50%"}>
+                <img src={articlesData?.articles[0]?.squareThumbnail} />
+              </C_ImgBox>
+            </Bottom>
+          </Right>
+        </ContentBox>
+      </Container>
+    </C_Section>
+  );
+};
 export default SectionOne;
